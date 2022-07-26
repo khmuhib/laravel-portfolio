2a143b509590fbd0184a4fb069374462bfc3aff5
khmuhib <khmuhib2013@gmail.com>
2022-07-26 17:55:23 +0600

    class - 11 complete

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -68,23 +68,55 @@
                         <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
                             data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="{{ route('admin.services.create') }}">Create</a>
-                                <a class="nav-link" href="{{ route('admin.services.list') }}">List</a>
+                                <a class="nav-link" href="{{ route('admin.services.create') }}">Add Service</a>
+                                <a class="nav-link" href="{{ route('admin.services.list') }}">Service List</a>
+                            </nav>
+                        </div>
+                        {{-- Portfolio --}}
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts1">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                            Portfolio
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="">Add Portfolio</a>
+                                <a class="nav-link" href="">Portfolio List</a>
                             </nav>
                         </div>
 
-                        <a class="nav-link" href="{{ route('admin.main') }}">
-                            <div class="sb-nav-link-icon"><i class="fa-solid fa-file-word"></i></div>
-                            Protfolio
-                        </a>
-                        <a class="nav-link" href="{{ route('admin.main') }}">
-                            <div class="sb-nav-link-icon"><i class="fa-solid fa-address-card"></i></div>
+                        {{-- About --}}
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayouts2" aria-expanded="false"
+                            aria-controls="collapseLayouts2">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                             About
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <a class="nav-link" href="{{ route('admin.main') }}">
-                            <div class="sb-nav-link-icon"><i class="fa-solid fa-anchor-circle-check"></i></div>
+                        <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="">Add About</a>
+                                <a class="nav-link" href="">Show About Data</a>
+                            </nav>
+                        </div>
+                        {{-- Contact --}}
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayouts3" aria-expanded="false"
+                            aria-controls="collapseLayouts3">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                             Contact
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
+                        <div class="collapse" id="collapseLayouts3" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="">Add Contact Info</a>
+                                <a class="nav-link" href="">See Massage</a>
+                            </nav>
+                        </div>
                     </div>
                 </div>
                 <div class="sb-sidenav-footer">
